7a27923d102065914ab2a4dcc9f6adc83681bfc8
manorajesh <mano@raje.sh>
2025-06-04 00:44:45 -0700

    Get spectral representation using custom generated spectral mapping file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use rgb2spec::{ RGB2Spec, eval_precise };
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let model = RGB2Spec::load("srgb_64.spec")?;
+    let rgb = [0.8, 0.3, 0.1];
+    let coeffs = model.fetch(rgb);
+    let λ = 550.0;
+    let refl = eval_precise(coeffs, λ);
+
+    println!("Reflectance at {} nm: {}", λ, refl);
+
+    Ok(())
 }
